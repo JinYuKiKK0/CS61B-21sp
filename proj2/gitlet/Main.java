adaf49915f30e2d8db36e8adcdc9ef250ef380e7
JinYu <90668645+JinYuKiKK0@users.noreply.github.com>
2025-01-09 11:19:29 +0800

    commit finished and change  main

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -13,33 +13,42 @@ public class Main {
      * Usage: java gitlet.Main ARGS, where ARGS contains
      * <COMMAND> <OPERAND1> <OPERAND2> ...
      */
-    public static void main(String[] args) throws IOException {
-        // TODO: what if args is empty?
-        String firstArg = args[0];
-        if (firstArg == null) {
-            System.out.println("Please enter a command.");
-            System.exit(0);
-        }
-        switch (firstArg) {
-            case "init":
-                // TODO: handle the `init` command
-                Repository.init();
-                break;
-            case "add":
-                // TODO: handle the `add [filename]` command
-                Repository.add(args[1]);
-                break;
-            // TODO: FILL THE REST IN
-            case "commit":
-                if (args[1] == null) {
-                    System.out.println("Please enter a commit message.");
+    public static void main(String[] args)  {
+        try {
+            // TODO: what if args is empty?
+            if (args.length == 0) {
+                System.out.println("Please enter a command.");
+                System.exit(0);
+            }
+            String firstArg = args[0];
+            switch (firstArg) {
+                case "init":
+                    // TODO: handle the `init` command
+                    Repository.init();
                     break;
-                }
-                Repository.commit(args[1]);
-                break;
-            case "rm":
-                Repository.rm(args[1]);
-                break;
+                case "add":
+                    // TODO: handle the `add [filename]` command
+                    if(args.length < 2){
+                        System.out.println("Please specify a file to add.");
+                        System.exit(0);
+                    }
+                    Repository.add(args[1]);
+                    break;
+                // TODO: FILL THE REST IN
+                case "commit":
+                    if (args.length < 2) {
+                        System.out.println("Please enter a commit message.");
+                        System.exit(0);
+                    }
+                    Repository.commit(args[1]);
+                    break;
+                case "rm":
+                    Repository.rm(args[1]);
+                    break;
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while processing the command: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
